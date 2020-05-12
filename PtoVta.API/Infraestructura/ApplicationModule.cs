4d2bc88b08c4469ccec0518cd682456abaa38f27
aguilaDeOro <jorgeluis_illescas@hotmail.com>
2020-05-12 14:24:23 -0500

    Ajustes Finales Web API

--- PtoVta.API/Infraestructura/ApplicationModule.cs
+++ PtoVta.API/Infraestructura/ApplicationModule.cs
@@ -7,8 +7,12 @@ using PtoVta.Dominio.Agregados.Usuario;
 using PtoVta.Infraestructura.Repositorios.Colaborador;
 using PtoVta.Infraestructura.Repositorios.Modulo;
 using PtoVta.Infraestructura.Repositorios.Usuario;
+using PtoVta.Infraestructura.Transversales.Adaptador;
 using PtoVta.Infraestructura.Transversales.Autenticacion;
+using PtoVta.Infraestructura.Transversales.Log;
+using PtoVta.Infraestructura.TransversalesNET.Adaptador;
 using PtoVta.Infraestructura.TransversalesNET.Autenticacion;
+using PtoVta.Infraestructura.TransversalesNET.Log;
 
 namespace PtoVta.API.Infraestructura
 {
@@ -49,6 +53,11 @@ namespace PtoVta.API.Infraestructura
                 .As<IAutenticacion>()
                 .InstancePerLifetimeScope();    
                             
+
+                LogFactory.EstablecerActual(new LogTrazaOrigenFactory());
+
+                var adaptadorFactory = new AutomapperTipoAdaptadorFactory();
+                TipoAdaptadorFactory.EstablecerActual(adaptadorFactory);  
 
                 // builder.RegisterAssemblyTypes(typeof(CrearArticuloControladorComando).GetTypeInfo().Assembly)
                     //.AsClosedTypesOf(typeof(IIntegrationEventHandler<>))
